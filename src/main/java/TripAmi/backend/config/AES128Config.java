34ab9d37698c098dbec64b94c71860108ab1ea88
jaekkang <rkdwornjs123@gmail.com>
2024-01-08 15:00:52 +0900

    Refactor: Aes encryptAes edit name

--- src/main/java/TripAmi/backend/config/AES128Config.java
+++ src/main/java/TripAmi/backend/config/AES128Config.java
@@ -1,5 +1,6 @@
 package TripAmi.backend.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 @Component
+@Slf4j
 public class AES128Config {
     private static final Charset ENCODING_TYPE = StandardCharsets.UTF_8;
     private static final String INSTANCE_TYPE = "AES/CBC/PKCS5Padding";
@@ -31,18 +33,21 @@ public class AES128Config {
         byte[] iv = new byte[16];   // 16bytes = 128bits
         secureRandom.nextBytes(iv);
         ivParameterSpec = new IvParameterSpec(iv);
-        secretKeySpec = new SecretKeySpec(secretKey.getBytes(ENCODING_TYPE), "AES");
+        secretKeySpec = new SecretKeySpec(iv, "AES");
         cipher = Cipher.getInstance(INSTANCE_TYPE);
     }
 
+
     // AES 암호화
+
     public String encryptAes(String plaintext) {
         try {
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
-            byte[] encryted = cipher.doFinal(plaintext.getBytes(ENCODING_TYPE));
-            return new String(Base64.getEncoder().encode(encryted), ENCODING_TYPE);
+            byte[] encrypted = cipher.doFinal(plaintext.getBytes(ENCODING_TYPE));
+            return new String(Base64.getEncoder().encode(encrypted), ENCODING_TYPE);
         } catch (Exception e) {
             //todo 에러 생성
+            log.error(e.getMessage());
             throw new RuntimeException("ENCRYPTION_FAILED");
         }
     }
