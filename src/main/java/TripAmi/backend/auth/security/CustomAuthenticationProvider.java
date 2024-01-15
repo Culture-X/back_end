7ed090e077e8cb0e903038690c9d6ab15964029f
Danbi <ddanhopark@gmail.com>
2024-01-15 17:35:46 +0900

    Fix: login 인증 로직 수정 (#11)

--- src/main/java/TripAmi/backend/auth/security/CustomAuthenticationProvider.java
+++ src/main/java/TripAmi/backend/auth/security/CustomAuthenticationProvider.java
@@ -1,30 +1,38 @@
 package TripAmi.backend.auth.security;
 
 import TripAmi.backend.auth.authmember.domain.Role;
-import TripAmi.backend.auth.security.infra.CustomUserDetails;
 import TripAmi.backend.auth.security.infra.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @RequiredArgsConstructor
+@Slf4j
 public class CustomAuthenticationProvider implements AuthenticationProvider {
     private final CustomUserDetailsService customUserDetailsService;
 
     private final BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        log.debug("CustomAuthenticationProvider.authenticate");
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails user = customUserDetailsService.loadUserByUsername(email);
+        if (!passwordEncoder.matches(password, user.getPassword()))
+            throw new BadCredentialsException("BadCredentialsException");
         UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(Role.ROLE_MEMBER.name()));
