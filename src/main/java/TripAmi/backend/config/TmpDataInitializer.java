10ff78b56e57b2a3205bc1a3c55fe1067146db02
jaekkang <rkdwornjs123@gmail.com>
2024-01-16 16:54:38 +0900

    Feat: update tmp data

--- src/main/java/TripAmi/backend/config/TmpDataInitializer.java
+++ src/main/java/TripAmi/backend/config/TmpDataInitializer.java
@@ -9,6 +9,10 @@ import TripAmi.backend.app.product.domain.Program;
 import TripAmi.backend.app.product.domain.ProgramRepository;
 import TripAmi.backend.app.product.domain.Spot;
 import TripAmi.backend.app.product.domain.TransportCode;
+import TripAmi.backend.app.report.domain.Report;
+import TripAmi.backend.app.report.domain.ReportRepository;
+import TripAmi.backend.app.report.domain.ReportStatus;
+import TripAmi.backend.app.report.domain.ReportType;
 import TripAmi.backend.auth.authmember.domain.AuthMember;
 import TripAmi.backend.auth.authmember.domain.AuthMemberRepository;
 import TripAmi.backend.auth.authmember.domain.Role;
@@ -17,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -31,6 +36,7 @@ public class TmpDataInitializer {
     private final AuthMemberRepository authMemberRepository;
     private final PasswordEncoder passwordEncoder;
     private final BannerRepository bannerRepository;
+    private final ReportRepository reportRepository;
 
     @PostConstruct
     private void createBanner() {
@@ -43,8 +49,43 @@ public class TmpDataInitializer {
     }
 
     @PostConstruct
+    private void createReport() {
+        Report report = Report.builder()
+                            .authorId(1L)
+                            .type(ReportType.INCORRECT)
+                            .content("test report test reporttest reporttest reporttest reporttest report")
+                            .status(ReportStatus.OPEN)
+                            .build();
+
+        reportRepository.save(report);
+
+        Report report2 = Report.builder()
+                            .authorId(2L)
+                            .type(ReportType.ECT)
+                            .content("test report test reporttest reporttest reporttest reporttest report")
+                            .status(ReportStatus.READ)
+                            .build();
+
+        reportRepository.save(report2);
+
+        Report report3 = Report.builder()
+                             .authorId(3L)
+                             .type(ReportType.AMI_PROBLEM)
+                             .content("test report test reporttest reporttest reporttest reporttest report")
+                             .status(ReportStatus.PROCESSED)
+                             .build();
+
+        reportRepository.save(report3);
+
+    }
+
+    @PostConstruct
     private void createMembersAndPrograms() {
+        LocalDateTime time = LocalDateTime.now();
+
+
         for (Long i = 1L; i <= 3L; i++) {
+            time = time.plusDays(1L);
             Traveler traveler = new Traveler();
             Ami ami = new Ami();
 
@@ -63,12 +104,13 @@ public class TmpDataInitializer {
 
             // Create three programs for each member
             for (int j = 1; j <= 3; j++) {
-                createProgram("Program" + j, ami);
+                createProgram("Program" + j, ami, time);
+                time = time.plusDays(30L);
             }
         }
     }
 
-    private void createProgram(String title, Ami ami) {
+    private void createProgram(String title, Ami ami, LocalDateTime plusDay) {
         // Add two spots to the program
         Spot spot1 = Spot.builder()
                          .title("Spot 1 for " + title)
@@ -110,7 +152,7 @@ public class TmpDataInitializer {
                               .keywords(Arrays.asList("keyword1", "keyword2"))
                               .location("Program location for " + title)
                               .spots(Arrays.asList(spot1, spot2))
-                              .startTime(LocalDateTime.of(9999,12,31,12,42))
+                              .startTime(plusDay)
                               .build();
 
         spot1.setProgram(program);
